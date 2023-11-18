d0dbfb3e714cf580b80f32e9ca2f0c8d9665aa80
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-11-18 09:55:16 +0200

    Update cmmdcsecv.cpp - scor 100

--- cmmdcsecv.cpp
+++ cmmdcsecv.cpp
@@ -1,47 +1,62 @@
 #include <fstream>
+#define MAX_N 100000
+#define MAX_NR_PRIME 200
 using namespace std;
-int lgmax;
+int a[MAX_N], prim[MAX_NR_PRIME];
+int n, vmax, nrPrime, lgmax;
 void citesteDateleDeIntrare();
+void determinaNumerelePrimeMaiMiciDecat(int nrMax);
+void calculeazaLungimeaMaxima();
 void afiseazaDateleDeIesire();
 int main()
 {
 	citesteDateleDeIntrare();
+	determinaNumerelePrimeMaiMiciDecat(vmax+1);
+	calculeazaLungimeaMaxima();
 	afiseazaDateleDeIesire();
 	return 0;
 }
-int celMaiMareDivizorComun(int a, int b)
+void calculeazaLungimeaMaxima()
 {
-	while (b)
+	int i, j, lg;
+	for (i = 0; i < nrPrime; i++)
 	{
-		int r = a % b;
-		a = b;
-		b = r;
+		for (lg = j = 0; j < n; j++)
+			if (a[j] % prim[i] == 0)
+				lg++;
+			else
+			{
+				if (lg > lgmax)
+					lgmax = lg;
+				lg = 0;
+			}
+		if (lg > lgmax)
+			lgmax = lg;
 	}
-	return a;
+}
+void determinaNumerelePrimeMaiMiciDecat(int nrMax)
+{
+	int ciur[nrMax], i, j;
+	for (i = 0; i < nrMax; i++) ciur[i] = 0;
+	ciur[0] = ciur[1] = 1;
+	for (i = 2; i*i < nrMax; i++)
+		if (ciur[i] == 0)
+			for (j = i*i; j < nrMax; j += i)
+				ciur[j] = 1;
+	for (i = 2; i < nrMax; i++)
+		if (ciur[i] == 0)
+			prim[nrPrime++] = i;
 }
 void citesteDateleDeIntrare()
 {
 	ifstream fin("cmmdcsecv.in");
-	int n, cmmdc, i, x, lg;
 	fin >> n;
-	fin >> cmmdc;
-	lg = cmmdc > 1 ? 1 : 0;
-	for (i = 1; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		fin >> x;
-		cmmdc = celMaiMareDivizorComun(cmmdc, x);
-		if (cmmdc > 1)
-			lg++;
-		else
-		{
-			if (lg > lgmax)
-				lgmax = lg;
-			lg = x > 1 ? 1 : 0;
-			cmmdc = x;
-		}
+		fin >> a[i];
+		if (a[i] > vmax)
+			vmax = a[i];
 	}
-	if (lg > lgmax)
-		lgmax = lg;
 	fin.close();
 }
 void afiseazaDateleDeIesire()
